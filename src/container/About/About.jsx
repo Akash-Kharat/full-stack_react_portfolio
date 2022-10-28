69910edbe77114ec21ab5af2ad4410519cff74fb
Akash-Kharat <akashkharat1339@gmail.com>
2022-10-28 08:12:25 +0530

    done

---
+++ src/container/About/About.jsx
@@ -0,0 +1,50 @@
+import React, {useState, useEffect} from 'react'
+import { images } from '../../constants';
+import './About.scss';
+import {urlFor , Client} from '../../Client';
+import {motion} from 'framer-motion';
+// const abouts =[
+//   {title:'Web Development', description: 'I am a good Web Developer',imgUrl:images.about01},
+//   {title:'Frontend Development', description: 'I am a good Web Developer', imgUrl:images.about02},
+//   {title:'UI/UX', description: 'I am a good Web Developer',imgUrl:images.about03},
+//   {title:'MERN stack', description: 'I am a good Web Developer',imgUrl:images.about04},
+// ];
+const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type =="abouts"]'
+    Client.fetch(query)
+    .then((data) => setAbouts(data))
+  }, [])
+  
+
+  return (
+    <>
+      <h2 className='head-text'>
+        I Know that
+        <span> Good Design </span><br />
+        Means
+        <span> Good Bussiness</span>
+      </h2>
+
+      <div className='app__profiles'>
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{opacity:1}}
+            whileHover={{scale:1.1}}
+            transition={{duration: 0.5, type:'tween'}}
+            className="app__profiles-item"
+            key={about.title + index}
+          >
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
+            <h2 className='bold-text' style={{marginTop:20}}>{about.title}</h2>
+            <p className='p-text' style={{marginTop:10}}>{about.description}</p>
+          </motion.div>
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default About
